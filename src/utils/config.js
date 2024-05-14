be75ec8a14fce72d01ce6e758ed25c4612779303
Nunna Satya Surya Venkat <31370698+satyasuryavenkat@users.noreply.github.com>
2024-05-14 12:02:35 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,1 @@
-export const BASE_URL = 'http://localhost:4000/api/v1'
-// export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
+export const BASE_URL = 'https://voyagein-backend-8kj8evvjy-suryas-projects-ec2343b7.vercel.app:4000/api/v1'
